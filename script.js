3819ae47dab93c29e908e67a25c3037a72582821
Valeria Sanabria <valerisanabria@tdf.edu.ar>
2022-12-12 21:47:59 -0300

    consumiendoApi

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// listar todos los empleados con boton para editar/eliminar
-// crear un formulario para crear/editar un empleado
-// al hacer click en editar se completa el formulario con datos del empleado
 
-// elegir un empleado y ponerle mi nombre
-// pensar como modificar la ciudad de todos los empleados
+fetch('https://6393e57e11ed187986bf9667.mockapi.io/api/curso/employees')
+    .then(response => response.json())
+    .then(data => {
+        console.log(`Nombre: ${data[0].name} Apellido: ${data[0].city}`);
 
-https://6393e57e11ed187986bf9667.mockapi.io/api/curso/employees
+    })
+    .catch(error => console.error(error));
