85b4f3236f2dbf82f83cd4ddaecdbfd08da35925
Valeria Sanabria <valerisanabria@tdf.edu.ar>
2022-12-12 21:19:53 -0300

    ejemploApi

---
+++ ejemploEnClase/script.js
@@ -0,0 +1,37 @@
+let inputName = document.getElementById('name');
+let inputSurname = document.getElementById('surname');
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click', addUser)
+        
+function addUser(name) {
+    // creo los elementos dentro del tbody
+    let tr = document.createElement('tr');
+        let tdName = document.createElement('td');
+        let tdSurname = document.createElement('td');
+        
+        // le inserto los valores al tr y td
+        tdName.innerText = inputName.value;
+        tdSurname.innerText = inputSurname.value;
+
+        // agarro el tbody y voy insertando en el html 
+        let tbody = document.getElementById('tab');
+        tbody.appendChild(tr);
+        tr.appendChild(tdName);
+        tr.appendChild(tdSurname);
+}
+
+
+
+
+fetch('https://randomuser.me/api/?results=5')
+    .then(response => response.json())
+    .then(data => {
+        console.log(`Nombre: ${data.results[0].name.first} Apellido: ${data.results[0].name.last}`);
+
+        inputName.value = data.results[0].name.first;
+        inputSurname.value = data.results[0].name.last;
+
+        
+    })
+    .catch(err => console.error('Hubo un problem') + err.message);
---
+++ script.js
@@ -0,0 +1,8 @@
+// listar todos los empleados con boton para editar/eliminar
+// crear un formulario para crear/editar un empleado
+// al hacer click en editar se completa el formulario con datos del empleado
+
+// elegir un empleado y ponerle mi nombre
+// pensar como modificar la ciudad de todos los empleados
+
+https://6393e57e11ed187986bf9667.mockapi.io/api/curso/employees
